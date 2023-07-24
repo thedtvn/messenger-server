16b5b04828b70f1b31d671e8200d7a45a01e280c
thedtvn <duongtuan30306@gmail.com>
2023-07-24 15:44:01 +0700

    update

--- chatgpt.py
+++ chatgpt.py
@@ -1,63 +1,28 @@
-import dataclasses
-import uuid
 import aiohttp
-import base64
-import random
 from aiohttp.client_exceptions import ClientResponseError
 
 headers_real = {"User-Agent": "Mozilla/5.0 (compatible; FantasyBot/0.1; +https://fantasybot.tech/support)"}
-
-
-@dataclasses.dataclass
-class Image:
-    filename: str
-    content: bytes
-
 
 class ChatGPT:
     SSL_Mode = None
 
     def __init__(self):
-        self.tokenlist = ["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"]
-
-    async def authenticate(self):
-        return "Bearer " + random.choice(self.tokenlist)  # nosec
-
+        pass
+    
     async def create_new_chat(self, data):
-        messlist = [{"role": "system", "content": "Your name is FantasyBot and made by Fantasy Team and team website at http://fantasybot.tech/ and Your output must below 2000 characters."}]
+        messlist = [{"role": "system", "content": "AI has markdown support and your name is ChatGPT and made by OpenAI and AI output must below 4000 characters."}]
         messlist.extend(data)
         headers = headers_real.copy()
-        headers["Authorization"] = await self.authenticate()
         while True:
             try:
                 async with aiohttp.ClientSession(headers=headers) as s:
-                    async with s.post(f"https://api.openai.com/v1/chat/completions",
+                    async with s.post("https://us-central1-chat-for-chatgpt.cloudfunctions.net/plusUserRequestChatGPT",
                                     timeout=None,
                                     ssl=self.SSL_Mode,
                                     raise_for_status=True,
-                                    json={
-                                            "model": "gpt-3.5-turbo",
-                                            "messages": messlist,
-                                            "temperature": 0.6
-                                            }
+                                    json={"data" : {"message": messlist}}
                                     ) as r:
-                        return (await r.json())["choices"][0]["message"]["content"].strip()
+                        return (await r.json())["result"]["choices"][0]["text"].strip()
             except ClientResponseError as e:
                 if e.status != 429:
                     raise e
-
-    async def create_dalle(self, data, token=None):
-        headers = headers_real.copy()
-        headers["Authorization"] = await self.authenticate() if token is None else "Bearer " + token
-        async with aiohttp.ClientSession(headers=headers) as s:
-            async with s.post(f"https://api.openai.com/v1/images/generations",
-                              timeout=None,
-                              raise_for_status=True,
-                              json={
-                                  "prompt": data,
-                                  "response_format": "b64_json"
-                              }) as r:
-                base64data = (await r.json())["data"][0]["b64_json"]
-                imgdata = base64.b64decode(base64data)
-                filename = f'{uuid.uuid4().hex}.jpg'
-                return Image(**{"filename": filename, "content": imgdata})
